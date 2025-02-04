6547e0ee4997eecc52b7fda05a0bd8ef518f5565
Yurielle <69353142+Yowkylito@users.noreply.github.com>
2025-02-04 15:59:48 +0800

    Update test.kt

--- test.kt
+++ test.kt
@@ -10,13 +10,11 @@ import com.bployaltyapp.compose.model.register.RegisterIntentState
 import com.bployaltyapp.compose.model.register.RegisterState
 import com.bployaltyapp.compose.utils.toTitleCase
 import com.bployaltyapp.compose.values.neutral_100
-import com.bployaltyapp.data.repository.otp.OtpRepositoryImpl.Companion.TAG
 import com.bployaltyapp.data.repository.registration.RegistrationRepository
 import com.bployaltyapp.data.source.remote.request.RegistrationRequest
 import com.ldd.foundation.designsystem.secondary_red
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -27,84 +25,58 @@ import javax.inject.Inject
 @HiltViewModel
 class RegistrationViewModel
     @Inject
-    constructor(private val registrationRepository: RegistrationRepository) : ViewModel() {
+    constructor(
+        private val registrationRepository: RegistrationRepository,
+    ) : ViewModel() {
         private val _registerState = MutableStateFlow(RegisterState())
         val registerState: StateFlow<RegisterState> = _registerState
+
         private val _state = MutableStateFlow<RegisterIntentState>(RegisterIntentState.Idle)
         val state: StateFlow<RegisterIntentState> get() = _state
+
         private var currentMobileNumber: String = ""
 
         fun processRegisterIntent(intent: RegisterIntent) {
             when (intent) {
-                is RegisterIntent.EnterMobileNumber -> {
-                    numberValidation(intent.mobileNumber)
-                }
+                is RegisterIntent.EnterMobileNumber -> numberValidation(intent.mobileNumber)
                 is RegisterIntent.RegMobileNumber -> phoneNumberVerification(intent.mobileNumber)
-                is RegisterIntent.AgreedToPolicy -> validate(policy = intent.isChecked)
-                RegisterIntent.ValidationSuccess -> {
-                    successValidation()
-                }
-                RegisterIntent.ValidationFailed -> {
-                    failedValidation()
-                }
-                RegisterIntent.CredentialsReset -> {
-                    resetCredentials()
-                }
-
-                // OTP
-
-                is RegisterIntent.EnterOtp -> {
-                    currentMobileNumber = registerState.value.mobileNumber
-                    _registerState.update { it.copy(otp = intent.otp) }
-                    _state.value = RegisterIntentState.OtpInput(otp = intent.otp, mobileNumber = currentMobileNumber)
-                }
+                is RegisterIntent.AgreedToPolicy -> validatePolicy(intent.isChecked)
+                RegisterIntent.ValidationSuccess -> successValidation()
+                RegisterIntent.ValidationFailed -> failedValidation()
+                RegisterIntent.CredentialsReset -> resetCredentials()
+                is RegisterIntent.EnterOtp -> handleOtpInput(intent.otp)
                 is RegisterIntent.ResendOtp -> resendOtp(intent.phoneNumber)
                 is RegisterIntent.SubmitOtp -> submitOtp()
-
-                // Set Up Password screen
                 is RegisterIntent.EnterConfirmPassword -> confirmPassword(intent.confirmPassword)
-                is RegisterIntent.EnterFirstName -> firstName(intent.firstName)
-                is RegisterIntent.EnterLastName -> lastName(intent.lastName)
-                is RegisterIntent.EnterPassword -> passwordValidation(intent.password)
+                is RegisterIntent.EnterFirstName -> updateFirstName(intent.firstName)
+                is RegisterIntent.EnterLastName -> updateLastName(intent.lastName)
+                is RegisterIntent.EnterPassword -> validatePassword(intent.password)
                 is RegisterIntent.TextFieldFocus -> focusHandler(intent.tfName, intent.focused)
                 is RegisterIntent.Register -> registration(intent.reg)
-                is RegisterIntent.GetPhoneNumber -> getPhoneNumber(intent.phoneNumber)
+                is RegisterIntent.GetPhoneNumber -> updatePhoneNumber(intent.phoneNumber)
             }
-        }
-
-        private fun String.isMobileNumberValid(): Boolean {
-            return length == 10
         }
 
         private fun numberValidation(mobileNumber: String) {
             _registerState.update { it.copy(mobileNumber = "0$mobileNumber") }
             _registerState.value =
                 when {
-                    mobileNumber.isEmpty() ->
-                        _registerState.value.copy(
-                            isMobileNumValid = false,
-                        )
+                    mobileNumber.isEmpty() -> _registerState.value.copy(isMobileNumValid = false)
                     !mobileNumber.isMobileNumberValid() ->
                         _registerState.value.copy(
                             isMobileNumValid = false,
                             mobileNumberError = "Please enter a valid mobile number",
                         )
-                    else ->
-                        _registerState.value.copy(
-                            mobileNumberError = null,
-                            isMobileNumValid = true,
-                        )
+                    else -> _registerState.value.copy(isMobileNumValid = true, mobileNumberError = null)
                 }
         }
 
-        private fun validate(policy: Boolean) {
-            viewModelScope.launch {
-                _registerState.update { it.copy(isPolicyBoxChecked = policy) }
-            }
+        private fun validatePolicy(policy: Boolean) {
+            _registerState.update { it.copy(isPolicyBoxChecked = policy) }
         }
 
         private fun successValidation() {
-            _registerState.update { it.copy(isPolicyBoxChecked = false, isBottomSheetShow = false) }
+            _registerState.update { it.copy(isPolicyBoxChecked = false, isBottomSheetShow = false, isRegistrationSuccess = false) }
         }
 
         private fun resetCredentials() {
@@ -112,9 +84,7 @@ class RegistrationViewModel
         }
 
         private fun failedValidation() {
-            viewModelScope.launch {
-                _registerState.update { it.copy(isBottomSheetShow = true) }
-            }
+            _registerState.update { it.copy(isBottomSheetShow = true) }
         }
 
         private fun phoneNumberVerification(mobileNumber: String) {
@@ -122,27 +92,29 @@ class RegistrationViewModel
                 _state.value = RegisterIntentState.Loading
                 try {
                     val response = registrationRepository.sendOtp(mobileNumber)
-                    Log.i("Yowkey", "vm response $response")
                     if (response.code == 1) {
                         _state.value = RegisterIntentState.OtpSent(response.data)
-                        Log.i("Yowkey", "OTP sent successfully.")
                     } else {
-                        _state.value = RegisterIntentState.Error("Sending Failed")
-                        Log.i("Yowkey", "Error: Sending OTP failed with response code: ${response.code}")
+                        _registerState.update {
+                            it.copy(
+                                mobileNumberError = "Number Already Used",
+                                isMobileNumValid = false,
+                            )
+                        }
 
+                        _state.value = RegisterIntentState.Error("Sending Failed")
                     }
                 } catch (e: Exception) {
                     _state.value = RegisterIntentState.Error(e.message ?: "Unknown Error")
-                    Log.i("Yowkey", "Exception: ${e.message}")
                 }
             }
         }
 
-        private fun getPhoneNumber(phoneNumber: String) {
-            _registerState.update { it.copy(mobileNumber = phoneNumber) }
+        private fun handleOtpInput(otp: String) {
+            currentMobileNumber = registerState.value.mobileNumber
+            _registerState.update { it.copy(otp = otp) }
+            _state.value = RegisterIntentState.OtpInput(otp = otp, mobileNumber = currentMobileNumber)
         }
-
-        // OTP
 
         private fun submitOtp() {
             viewModelScope.launch {
@@ -150,21 +122,21 @@ class RegistrationViewModel
                 if (currentState is RegisterIntentState.OtpInput) {
                     _state.value = RegisterIntentState.Loading
                     try {
-                        val response = registrationRepository.verifyOtp(_registerState.value.mobileNumber, _registerState.value.otp)
+                        val response =
+                            registrationRepository.verifyOtp(
+                                _registerState.value.mobileNumber,
+                                _registerState.value.otp,
+                            )
                         if (response.code == 1) {
                             _state.value = RegisterIntentState.OtpVerified(response.data)
-                            Log.i(TAG, "OTP verified successfully.")
                         } else {
                             _state.value = RegisterIntentState.Error("Verification Failed")
-                            Log.i(TAG, "Error: Verification failed with response code: ${response.code}")
                         }
                     } catch (e: Exception) {
                         _state.value = RegisterIntentState.Error(e.message ?: "Unknown Error")
-                        Log.i(TAG, "Exception: ${e.message}")
                     }
                 } else {
                     _state.value = RegisterIntentState.Error("Invalid state for submitting OTP")
-                    Log.i(TAG, "Error: Invalid state for submitting OTP")
                 }
             }
         }
@@ -175,27 +147,22 @@ class RegistrationViewModel
                 if (currentState is RegisterIntentState.OtpInput) {
                     _state.value = RegisterIntentState.Loading
                     try {
-                        val response = registrationRepository.sendOtp(currentState.mobileNumber)
-                        delay(1000) // Simulate network delay
+                        val response = registrationRepository.sendOtp(phoneNumber)
                         if (response.code == 1) {
                             _state.value = currentState.copy(showResendSection = true)
-                            Log.i(TAG, "OTP resent successfully.")
                         } else {
                             _state.value = RegisterIntentState.Error("Resending Failed")
-                            Log.i(TAG, "Error: Resending OTP failed with response code: ${response.code}")
                         }
                     } catch (e: Exception) {
                         _state.value = RegisterIntentState.Error(e.message ?: "Unknown Error")
-                        Log.i(TAG, "Exception: ${e.message}")
                     }
                 } else {
                     _state.value = RegisterIntentState.Error("Invalid state for resending OTP")
-                    Log.i(TAG, "Error: Invalid state for resending OTP")
                 }
             }
         }
 
-        private fun firstName(firstName: String) {
+        private fun updateFirstName(firstName: String) {
             _registerState.update { it.copy(firstName = firstName.toTitleCase()) }
             _registerState.value =
                 when {
@@ -205,17 +172,16 @@ class RegistrationViewModel
                             firstNameBorder = secondary_red,
                             firstNameError = "First name can't be empty.",
                         )
-                    else -> {
+                    else ->
                         _registerState.value.copy(
                             isFirstNameValid = true,
                             firstNameBorder = neutral_100,
                             firstNameError = null,
                         )
-                    }
                 }
         }
 
-        private fun lastName(lastName: String) {
+        private fun updateLastName(lastName: String) {
             _registerState.update { it.copy(lastName = lastName.toTitleCase()) }
             _registerState.value =
                 when {
@@ -225,19 +191,17 @@ class RegistrationViewModel
                             lastNameBorder = secondary_red,
                             lastNameError = "Last name can't be empty.",
                         )
-                    else -> {
+                    else ->
                         _registerState.value.copy(
                             isLastNameValid = true,
                             lastNameBorder = neutral_100,
                             lastNameError = null,
                         )
-                    }
                 }
         }
 
-        private fun passwordValidation(password: String) {
+        private fun validatePassword(password: String) {
             _registerState.update { it.copy(password = password) }
-
             var isValid = true
             var errorMessage: String? = null
 
@@ -290,10 +254,51 @@ class RegistrationViewModel
                 it.copy(
                     isPasswordValid = isValid,
                     passwordError = if (isValid) null else errorMessage,
-                    passwordBorder = if (isValid && (password == _registerState.value.confirmPassword)) neutral_100 else secondary_red,
                 )
             }
         }
+
+        @SuppressLint("NewApi")
+        private fun registration(regData: RegistrationRequest) {
+            viewModelScope.launch(Dispatchers.IO) {
+                try {
+                    val response = registrationRepository.registerAccount(regData)
+                    if (response.code == 1) {
+                        _registerState.update { it.copy(isRegistrationSuccess = true, response = response.toString()) }
+                    }
+                } catch (
+                    @SuppressLint("NewApi") e: HttpException,
+                ) {
+                    Log.e("ERROR", "HTTP Exception: ${e.message}", e)
+                } catch (e: IOException) {
+                    Log.e("ERROR", "Network Exception: ${e.message}", e)
+                } catch (e: Exception) {
+                    Log.i("ERROR", "Unexpected Exception: ${e.message} for ${_registerState.value.response}")
+                }
+            }
+        }
+
+        private fun focusHandler(
+            textFieldName: String,
+            focused: Boolean,
+        ) {
+            when (textFieldName.lowercase()) {
+                "firstname" -> {
+                    _registerState.value = _registerState.value.copy(isFirstnameFocus = focused)
+                }
+                "lastname" -> {
+                    _registerState.value = _registerState.value.copy(isLastnameFocus = focused)
+                }
+            }
+        }
+
+        private fun updatePhoneNumber(phoneNumber: String) {
+            _registerState.update { it.copy(mobileNumber = phoneNumber) }
+        }
+
+        private fun String.isMobileNumberValid(): Boolean = length == 10
+
+        private fun String.hasSpecialCharacter(): Boolean = any { !it.isLetterOrDigit() }
 
         private fun confirmPassword(confirmPassword: String) {
             if (confirmPassword == _registerState.value.password) {
@@ -319,43 +324,6 @@ class RegistrationViewModel
                         passwordError = "Password not matched",
                         passwordBorder = secondary_red,
                     )
-                }
-            }
-        }
-
-        @SuppressLint("NewApi")
-        private fun registration(regData: RegistrationRequest) {
-            viewModelScope.launch(Dispatchers.IO) {
-                try {
-                    val response = registrationRepository.registerAccount(regData)
-                    Log.i("YOWKEY", "RESPONSE: $response DATA: $regData")
-                } catch (
-                    @SuppressLint("NewApi") e: HttpException,
-                ) {
-                    Log.e("YOWKEY", "HTTP Exception: ${e.message}", e)
-                } catch (e: IOException) {
-                    Log.e("YOWKEY", "Network Exception: ${e.message}", e)
-                } catch (e: Exception) {
-                    Log.e("YOWKEY", "Unexpected Exception: ${e.message}", e)
-                }
-            }
-        }
-
-        private fun String.hasSpecialCharacter(): Boolean {
-            val specialCharacters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\`~"
-            return any { it in specialCharacters }
-        }
-
-        private fun focusHandler(
-            textFieldName: String,
-            focused: Boolean,
-        ) {
-            when (textFieldName.lowercase()) {
-                "firstname" -> {
-                    _registerState.value = _registerState.value.copy(isFirstnameFocus = focused)
-                }
-                "lastname" -> {
-                    _registerState.value = _registerState.value.copy(isLastnameFocus = focused)
                 }
             }
         }
